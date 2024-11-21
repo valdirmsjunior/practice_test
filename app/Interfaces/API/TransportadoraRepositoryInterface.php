abf752fe3fc667b612ec5223e21b93a37187ec20
Valdir Júnior <valdirmsjunior@gmail.com>
2024-11-21 13:33:13 -0300

    corrigindo o delete

--- app/Interfaces/API/TransportadoraRepositoryInterface.php
+++ app/Interfaces/API/TransportadoraRepositoryInterface.php
@@ -9,6 +9,6 @@ interface TransportadoraRepositoryInterface
     public function getAll();
     public function create(array $data);
     public function update(array $data, Transportadora $transportadora);
-    public function delete(Transportadora $transportadora);
+    public function delete($id);
     public function findById($id);
 }
